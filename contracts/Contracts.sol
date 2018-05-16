6eecded0779f1b4b18122cee782c513600038c22
Mikhail Sokoluk <sokolukm@gmail.com>
2018-05-17 00:09:32 +0500

    some changes

--- contracts/Contracts.sol
+++ contracts/Contracts.sol
@@ -1,7 +1,9 @@
 pragma solidity ^0.4.17;
 
 contract Contracts {
-    // Model a Candidate
+
+    address Admin;
+    // Model a Contract
     struct Contract {
         uint id;
         string contractNum;
@@ -17,50 +19,70 @@ contract Contracts {
         string article;
         uint amount;
     }
-    Contract[] public contracts;          // All contracts
-    mapping(uint => Contract) public contractById;
+    // Contract[] public contracts;          // All contracts
+    mapping(uint => Contract) public contracts;
+    uint public contractCount;
 
-    constructor () public {
-
+    constructor () payable {
+        Admin = msg.sender;
     }
     // Create an auction, transfer the item to this contract, activate the auction
     function createContract (
         string _contractNum,
         string _description,
-        uint _amount,
-        address _contractAddress
+        uint _amount
     ) public
         returns (uint contractId) {
-        contractId = contracts.length++;
-        Contract c = contracts[contractId];
+        contractCount ++;
+        Contract storage c = contracts[contractId];
         c.contractNum = _contractNum;
         c.description = _description;
-        c.timestamp =  now;
+        c.timestamp = now;
         c.amount = _amount;
-        c.contractAddress = _contractAddress;
+        c.contractAddress = msg.sender;
 
-        return contractId;
+        contracts[contractCount] = c;
+        return contractCount;
     }
 
-    function getContract(uint idx) public returns (
+    function getContract(uint idx) public constant returns (
         string,
         string,
         uint,
         uint,
         address) {
-        Contract c = contracts[idx];
-
-        return (c.contractNum,
-                c.description,
-                c.timestamp,
-                c.amount,
-                c.contractAddress
+        return (
+            contracts[idx].contractNum,
+            contracts[idx].description,
+            contracts[idx].timestamp,
+            contracts[idx].amount,
+            contracts[idx].contractAddress
         );
     }
 
     function getContractCount() public returns (uint) {
-        return contracts.length;
+        return contractCount;
     }
 
+    function updateContract(
+        uint idx,
+        string _contractNum,
+        string _description,
+        uint _amount
+        ) public returns (bool) 
+    {
+        Contract storage c = contracts[idx];
+        require (msg.sender == c.contractAddress, "Only owner can change");
 
+        c.contractNum = _contractNum;
+        c.description = _description;
+        c.timestamp = now;
+        c.amount = _amount;
+        c.contractAddress = msg.sender;
+        return true;
+    }
+
+    function getContractCount() public returns (uint) {
+        return contractCount;
+    }
 }
--- truffle.js
+++ truffle.js
@@ -1,16 +1,16 @@
 module.exports = {
     // See <http://truffleframework.com/docs/advanced/configuration>
     // for more about customizing your Truffle configuration!
-    rpc: {
-        host: "localhost",
-        port: 8545
-    },
+    // rpc: {
+    //     host: "localhost",
+    //     port: 8545
+    // },
     networks: {
       development: {
         host: "127.0.0.1",
-        // port: 7545,
-        port: 8545,
-        network_id: "1999", // Match any network id
+        port: 7545,
+        // port: 8545,
+        network_id: "*", // Match any network id
         gas: 4698712
       }
     }
