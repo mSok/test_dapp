f4c84806c8859d927caf78cb36f94449a8a3088d
Mikhail Sokoluk <sokolukm@gmail.com>
2018-05-26 21:54:28 +0500

    css for mobile

--- src/js/BetContract.vue
+++ src/js/BetContract.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="row flex-xl-nowrap justify-content-center mt-4">
-      <div class="col-11">
+      <div id="no-more-tables" class="col-11">
             <table class="table table-sm">
                 <thead class="thead-dark">
-                    <tr class="d-flex">
+                    <tr>
                         <th scope="col" class="col-1" >Номер контракта</th>
                         <th scope="col" class="col-3" >Описание</th>
                         <th scope="col" class="col-1" >Дата</th>
@@ -14,14 +14,14 @@
                     </tr>
                 </thead>
                 <tbody>
-                    <tr v-for="item in rateData" class="d-flex">
-                        <td class = "col-sm-1">{{item[1][1]}}</td>
-                        <td class = "col-sm-3">{{item[1][2]}}</td>
-                        <td class = "col-sm-1">{{new Date(item[1][3] * 1000).toLocaleDateString("ru-RU", options)}}</td>
-                        <td class = "col-sm-1">{{parseInt(item[1][4])}}</td>
-                        <td class = "col-sm-2">{{item[1][5]}}</td>
-                        <td class = "col-sm-1">{{parseInt(item[3])}}</td>
-                        <td class = "col-sm-2">{{item[2]}}</td>
+                    <tr v-for="item in rateData">
+                        <td data-title="Номер контракта" >{{item[1][1]}}</td>
+                        <td data-title="Описание">{{item[1][2]}}</td>
+                        <td data-title="Дата" >{{new Date(item[1][3] * 1000).toLocaleDateString("ru-RU", options)}}</td>
+                        <td data-title="Цена" >{{parseInt(item[1][4])}}</td>
+                        <td data-title="Владелец контракта" >{{item[1][5]}}</td>
+                        <td data-title="Ставка" >{{parseInt(item[3])}}</td>
+                        <td data-title="Участник">{{item[2]}}</td>
                     </tr>
                 </tbody>
             </table>
--- src/js/Dashboard.vue
+++ src/js/Dashboard.vue
@@ -3,6 +3,7 @@
     <div class="row flex-xl-nowrap justify-content-center mt-4">
       <div class="col-11">
         <GenerateContract @createContract="onCreateContract"> </GenerateContract>
+        <div id="no-more-tables">
         <table class="table table-sm">
             <thead class="thead-dark">
                 <tr>
@@ -18,20 +19,21 @@
             </thead>
             <tbody>
                 <tr v-for="item in contractsData">
-                    <th>{{parseInt(item[0])}}</th>
-                    <td>{{item[1]}}</td>
-                    <td>{{item[2]}}</td>
-                    <td>{{new Date(item[3] * 1000).toLocaleDateString("ru-RU", options)}}</td>
-                    <td>{{parseInt(item[4])}}</td>
-                    <td>{{item[5]}}</td>
-                    <td>{{item[6]}}</td>
-                    <td>
+                    <td data-title="#">{{parseInt(item[0])}}</td>
+                    <td data-title="Номер контракта">{{item[1]}}</td>
+                    <td data-title="Описание">{{item[2]}}</td>
+                    <td data-title="Дата">{{new Date(item[3] * 1000).toLocaleDateString("ru-RU", options)}}</td>
+                    <td data-title="Цена">{{parseInt(item[4])}}</td>
+                    <td data-title="Владелец">{{item[5]}}</td>
+                    <td data-title="Закрыт">{{item[6]}}</td>
+                    <td data-title="-">
                         <router-link exact :to="{name: 'setbet', params: { contract_id: parseInt(item[0]) }}">Сделать cтавку</router-link>
                     </td>
                 </tr>
             </tbody>
         </table>
-    </div>
+        </div>
+      </div>
     </div>
 </template>
 
@@ -136,6 +138,6 @@ export default {
   white-space: nowrap;
   overflow: hidden;
   text-overflow: ellipsis;
-
 }
+
 </style>
