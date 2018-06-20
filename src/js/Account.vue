ca75a98b60f5bb87afcadf59f1089379140aebde
Mikhail Sokoluk <sokolukm@gmail.com>
2018-06-20 21:48:11 +0500

    return TX id immediately, dont wait mining

--- src/js/Account.vue
+++ src/js/Account.vue
@@ -1,18 +1,17 @@
 <template>
     <div>
-        <div class="text-center">
-            <p class="lead text-muted">Введите Email, он будет сохранен и привязан к вашему кошельку</p>
-        </div>
-    <div class="row pb-2">
-        <div class="col-12">
-            <div class="form-row justify-content-center pt-4">
-                <input v-model="email" type="text" placeholder="Email">
-                <input v-model="nick" type="text" class="ml-2" placeholder="Nickname">
-                <button @click="setEmail" class="btn-primary ml-2">Тадам!</button>
-            </div>
-        </div>
-        <button @click="signMessage" class="btn-primary ml-2">ntcn!</button>
-    </div>
+      <div class="text-center">
+        <p class="lead text-muted">Введите Email, он будет сохранен и привязан к вашему кошельку</p>
+      </div>
+      <div class="row pb-2">
+          <div class="col-12">
+              <div class="form-row justify-content-center pt-4">
+                  <input v-model="email" type="text" placeholder="Email">
+                  <input v-model="nick" type="text" class="ml-2" placeholder="Nickname">
+                  <button @click="setEmail" class="btn-primary ml-2">Тадам!</button>
+              </div>
+          </div>
+      </div>
     </div>
 </template>
 
--- src/js/utils/token.js
+++ src/js/utils/token.js
@@ -37,10 +37,27 @@ export function getTokenRate (tokenContract = undefined) {
 export function buyTokens (amount) {
   return getCoinbase().then(selfAccount => {
     return initWeb3('crowdSale').then((Contracts) => {
-      return Contracts.methods.buyTokens(selfAccount).send({
-        from: selfAccount,
-        value: amount
+      return new Promise((resolve, reject) => {
+        // возвращаем сразу tx id не дождавшись майнинга
+        return Contracts.methods.buyTokens(selfAccount).send({
+          from: selfAccount,
+          value: amount
+        },
+        (error, hash) => {
+          if (error) reject(error)
+          console.log('TX immeditly : ', hash)
+          resolve(hash)
+        })
       })
+      // вариант ниже вернёт promise TX object только после майнинга транзакции(1-2 мин)
+      // return Contracts.methods.buyTokens(selfAccount).send({
+      //   from: selfAccount,
+      //   value: amount
+      // },
+      // (error, hash) => {
+      //   console.log('TX immeditly : ', hash)
+      //   return hash
+      // })
     })
   })
 }
