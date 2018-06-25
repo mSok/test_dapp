656c0d06636d11c740f99d0202cadc4e63994dd7
Mikhail Sokoluk <sokolukm@gmail.com>
2018-06-25 23:16:11 +0500

    добавил эфир

--- contracts/SitisServicePlacemarket.sol
+++ contracts/SitisServicePlacemarket.sol
@@ -2,11 +2,13 @@
 pragma solidity ^0.4.17;
 
 contract SitisPlaceMarket {
+
+    // Address where funds are collected
+    address public wallet;
     // услуга/товар
     struct Service {
         uint id;
         string serviceHash;
-        // uint timestamp;
         uint amount;
         address serviceOwner;
         bool closed;
@@ -15,10 +17,9 @@ contract SitisPlaceMarket {
     struct purchasedService {
         uint id;
         uint serviceId;
-        // uint timestamp;
         uint cnt;
         address buyer;
-        bool closed;
+        bool closed;  // совершенные
     }
     // маппинг услуг
     mapping(uint => Service) public services;
@@ -30,7 +31,9 @@ contract SitisPlaceMarket {
 
     event buyServiceEvent (
         address indexed _buyerAddresss,
-        uint _purchaseId
+        uint _purchaseId,
+        address indexed _serviceOwner,
+        uint256 _value
     );
     // маппинг покупок по покупателю (адрес покупателя => id покупки)
     // mapping(address => uint) public buyerPurchases;
@@ -43,15 +46,14 @@ contract SitisPlaceMarket {
         serviceCount ++;
         Service storage c = services[serviceCount];
         c.id = serviceCount;
-        // c.timestamp = now;
-        c.amount = _amount;
+        c.amount = _amount;  // стоимость за единицу
         c.serviceHash = _serviceHash;
         c.closed = false;
         c.serviceOwner = msg.sender;
         services[serviceCount] = c;
         return serviceCount;
     }
-    // закрыть услугу
+    // закрыть услугу, больше не предоставлять.
     function closeService (
         uint _serviceId
     ) public
@@ -62,24 +64,68 @@ contract SitisPlaceMarket {
         services[_serviceId] = c;
         return serviceCount;
     }
+    // закрыть покупку, перечислить средства продавцу.
+    function closePurchase (
+        uint _purchaseId
+    ) public {
+        purchasedService storage pc = purchases[_purchaseId];
+        require(msg.sender == pc.buyer);
+        require(pc.id != 0);
+        Service storage c = services[pc.serviceId];
+        uint256 amount = pc.cnt * c.amount;
+        c.serviceOwner.transfer(amount);
+        pc.closed = true;
+        purchases[_purchaseId] = pc;
+    }
     // купить услугу
     function buyService (
         uint _serviceId,
         uint _cnt
-    ) public
+    ) public payable
         returns (uint purchasesId) {
         Service storage c = services[_serviceId];
-        require(c.id >= 0);
+        _preValidatePurchase(
+            c.serviceOwner,
+            msg.value,
+            c.id,
+            _cnt
+        );
         require(c.closed == false);
         purchasesCount ++;
         purchasedService storage pc = purchases[purchasesCount];
         pc.serviceId = _serviceId;
-        // pc.timestamp = now;
         pc.cnt = _cnt;
         pc.buyer = msg.sender;
         pc.closed = false;
         purchases[purchasesCount] = pc;
-        buyServiceEvent(msg.sender, purchasesCount);
+        _forwardFunds();
+
+        emit buyServiceEvent(
+            msg.sender,
+            purchasesCount,
+            c.serviceOwner,
+            msg.value
+        );
         return purchasesCount;
+    }
+    function _forwardFunds() internal {
+        address(this).transfer(msg.value);
+    }
+    // validate purchase
+    function _preValidatePurchase(
+        address _beneficiary,
+        uint256 _weiAmount,
+        uint256 _serviceId,
+        uint256 _purchasesCount
+    )
+        internal
+    {
+        require(_beneficiary != address(0));
+        require(_weiAmount != 0);
+        require(_serviceId != 0);
+        Service storage c = services[_serviceId];
+        require(c.id != 0);
+        require(_purchasesCount != 0);
+        require(c.amount * _purchasesCount <= _weiAmount);
     }
 }
